73f1e13b93d794557f3c3f966eb75b1cd696acdc
Tyler Barnett <tyler.barnett7190@gmail.com>
2018-02-03 11:20:05 -0600

    added belong solution

---
+++ belong.js
@@ -0,0 +1,17 @@
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num)
+      return a;
+  }
+
+  return arr.length;
+}
+getIndexToIns([30, 40, 60, 70], 50);
